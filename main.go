0c4118ce9ac9a082642e291c4561e19ecbb16006
Ridin Datta <ridindatta12@gmail.com>
2024-04-29 06:05:13 +0530

    added another endpoint

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ import (
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
 }
+func api(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API"))
+}
 
 func main(){
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/api", api)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
